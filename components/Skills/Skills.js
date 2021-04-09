76c9eefd0ce0e21cc42e32070dbe43d5786927bb
Reubert Barbosa <reubertfernandes@gmail.com>
2021-04-09 01:53:46 -0300

    adding chars

--- components/Skills/Skills.js
+++ components/Skills/Skills.js
@@ -1,7 +1,7 @@
 import styles from "./Skills.module.css";
 
 const skills = [
-  { id: 1, title: "HTML & (S)CSS" },
+  { id: 1, title: "HTML && (S)CSS" },
   { id: 2, title: "(Type, Java)Script" },
   { id: 3, title: "React" },
   { id: 4, title: "REST APIs" },
@@ -9,7 +9,7 @@ const skills = [
   { id: 6, title: "Graphql" },
   { id: 7, title: "Jest" },
   { id: 8, title: "React Testing Library" },
-  { id: 9, title: "Git & Git(hub,lab)" },
+  { id: 9, title: "Git && Git(hub,lab)" },
 ];
 
 export default function Skills() {
